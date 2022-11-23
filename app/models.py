d46dbc37ef540ff2dc76d6f5cb4c8d663c30902a
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-24 00:11:38 +0300

    Add models

---
+++ app/models.py
@@ -0,0 +1,36 @@
+"""
+Describe all models at test case
+"""
+from app import db
+
+
+
+products_categories = db.Table('products_categories',
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id')),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id')))
+
+
+class Product(db.Model):
+    """
+    Represent model for Product
+    """
+    __tablename__ = 'product'
+
+    id = db.Column(db.Integer, primary_key=True)
+    prod_name = db.Column(db.String(50))
+
+    categories = db.relationship("Category", secondary=products_categories,
+                                backref=db.backref('products', lazy='dynamic'), lazy='dynamic')
+
+
+class Category(db.Model):
+    """
+    Model for Category
+    """
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cat_name = db.Column(db.String(50), unique=True)
+
+    products = db.relationship("Product", secondary=products_categories,
+                                backref=db.backref('categories', lazy='dynamic'), lazy='dynamic')
--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config(object):
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     FLASK_HOST = os.environ.get('FLASK_HOST') or '127.0.0.1'
-    FLASK_PORT = os.environ.get('FLASK_RUN_PORT') or '6000'
+    FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT') or '6000'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL') \
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
